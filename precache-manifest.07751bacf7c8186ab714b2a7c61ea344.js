134a04f553cb50197229a45f936b759c903edbe6
Al-Aswad <73427522+Al-Aswad@users.noreply.github.com>
2021-10-31 12:23:21 +0800

    deploy-test

---
+++ precache-manifest.07751bacf7c8186ab714b2a7c61ea344.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292ccc6418422ce8565d",
+    "url": "/css/app.79c03e4c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "00968fa5b87c81af59c8df86800bc8a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd3d868e1e2757b54831",
+    "url": "/js/about.ca9d9639.js"
+  },
+  {
+    "revision": "292ccc6418422ce8565d",
+    "url": "/js/app.a2a5dfca.js"
+  },
+  {
+    "revision": "be3e9f6bcb68c8b9ced4",
+    "url": "/js/chunk-vendors.d1718735.js"
+  },
+  {
+    "revision": "ad9200d8452827212858b375d349aed6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
